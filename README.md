ad733932857fce7fdd54395361ab7a443cbc528a
pR0Ps <pR0Ps.CM@gmail.com>
2014-03-28 12:21:17 -0400

    Initial commit with basic structure

---
+++ README.md
@@ -0,0 +1,4 @@
+GH-I2P
+======
+
+GitHub I2P is a web application that will convert an issue on GitHub to a pull request.
---
+++ app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from bottle import Bottle, route, run, request
+
+app = application = Bottle()
+
+HTMLBLOB = """
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta name="description" content="Github Issue to Pull Request converter">
+    <meta name="keywords" content="git, github, issue, pull pequest, convert">
+    <title>GH-I2P</title>
+    <style type="text/css">
+    body {{
+      color: #000000;
+      background-color: #FFFFFF;
+      text-decoration: none;
+      font-family: 'arial', 'sans-serif';
+    }}
+    main {{
+      text-align: center;
+      margin-top: 100px;
+    }}
+    a#title{{
+      color: #000000;
+      text-decoration: none;
+    }}
+    img#gh-banner {{
+      position: absolute;
+      top: 0;
+      right: 0;
+      border: 0;
+    }}
+    footer {{
+      position: absolute;
+      bottom: 50px;
+      right: 50px;
+    }}
+    </style>
+  </head>
+  <body>
+    <main>
+      <a href="/" id='title'><h1>Github - Issue2Pull</h1></a>
+      <a href="https://github.com/pR0Ps/gh-i2p"><img id="gh-banner" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+    </main>
+    <footer>Made by <a href="http://cmetcalfe.ca">Carey Metcalfe</a></footer>
+</html>"""
+
+@app.route('/')
+def index():
+    return HTMLBLOB.format()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
